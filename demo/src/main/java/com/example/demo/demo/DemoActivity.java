ab0f337846b8fe2f03591366628ec26212499602
Guillaume Delente <guillaume.delente+github@gmail.com>
2014-08-07 16:18:38 -0700

    Setup project and add logging

---
+++ demo/src/main/java/com/example/demo/demo/DemoActivity.java
@@ -0,0 +1,95 @@
+package com.example.demo.demo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import slidinguppanel.sothree.com.androidslidinguplist.SlidingUpPanelLayout;
+
+public class DemoActivity extends Activity {
+    private static final String TAG = "DemoActivity";
+
+    public static final String SAVED_STATE_ACTION_BAR_HIDDEN = "saved_state_action_bar_hidden";
+
+    private SlidingUpPanelLayout mLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+        setContentView(R.layout.activity_demo);
+
+        mLayout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
+        mLayout.setEnableDragViewTouchEvents(true);
+
+        ListView lv = (ListView) findViewById(R.id.listview);
+        lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                new String[]{"Plop", "Plip","Plop", "Plip","Plop", "Plip","Plop", "Plip",
+                        "Plop", "Plip","Plop", "Plip","Plop", "Plip","Plop", "Plip","Plop",
+                        "Plip","Plop", "Plip","Plop", "Plip","Plop", "Plip","Plop", "Plip",
+                        "Plop", "Plip","Plop", "Plip","Plop", "Plip","Plop", "Plip","Plop",
+                        "Plip","Plop", "Plip","Plop", "Plip","Plop", "Plip","Plop", "Plip",
+                        "Plop", "Plip","Plop", "Plip","Plop", "Plip",}));
+
+
+        boolean actionBarHidden = savedInstanceState != null && savedInstanceState.getBoolean(SAVED_STATE_ACTION_BAR_HIDDEN, false);
+        if (actionBarHidden) {
+            int actionBarHeight = getActionBarHeight();
+            setActionBarTranslation(-actionBarHeight);//will "hide" an ActionBar
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(SAVED_STATE_ACTION_BAR_HIDDEN, mLayout.isPanelExpanded());
+    }
+
+    private int getActionBarHeight(){
+        int actionBarHeight = 0;
+        TypedValue tv = new TypedValue();
+        if (getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
+            actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data,getResources().getDisplayMetrics());
+        }
+        return actionBarHeight;
+    }
+
+    public void setActionBarTranslation(float y) {
+        // Figure out the actionbar height
+        int actionBarHeight = getActionBarHeight();
+        // A hack to add the translation to the action bar
+        ViewGroup content = ((ViewGroup) findViewById(android.R.id.content).getParent());
+        int children = content.getChildCount();
+        for (int i = 0; i < children; i++) {
+            View child = content.getChildAt(i);
+            if (child.getId() != android.R.id.content) {
+                if (y <= -actionBarHeight) {
+                    child.setVisibility(View.GONE);
+                } else {
+                    child.setVisibility(View.VISIBLE);
+                    child.setTranslationY(y);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mLayout != null && mLayout.isPanelExpanded() || mLayout.isPanelAnchored()) {
+            mLayout.collapsePanel();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
